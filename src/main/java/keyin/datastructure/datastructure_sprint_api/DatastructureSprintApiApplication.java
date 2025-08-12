e263368faf7455a67672a658700a4c5417e8bff2
Gerogie18 <jennlyver@gmail.com>
2025-08-12 00:27:28 -0230

    feat: Add initial Spring Boot API project and CI workflow

---
+++ src/main/java/keyin/datastructure/datastructure_sprint_api/DatastructureSprintApiApplication.java
@@ -0,0 +1,13 @@
+package keyin.datastructure.datastructure_sprint_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructureSprintApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructureSprintApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/keyin/datastructure/datastructure_sprint_api/DatastructureSprintApiApplicationTests.java
@@ -0,0 +1,13 @@
+package keyin.datastructure.datastructure_sprint_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructureSprintApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
